e068f9f5b0e47b2cee3aafd2563958efb022635a
Tom <goofytompo@gmail.com>
2022-06-03 01:49:54 +0100

    decents shit

--- get_data.py
+++ get_data.py
@@ -1,52 +1,66 @@
 from __future__ import print_function
 
-import os.path
-
-from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-SPREADSHEET_ID = "1fJmdaoYiMquDwgxFETxv2Ig4A_Qon9lZSsDwnR8malw"
-RANGE_NAME = "Drivers Standings!A2:C"
-
-def main():
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
+class get_data:
+    def __init__(self, SPREADSHEET_ID):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    
-    try:
         service = build('sheets', 'v4', credentials=creds)
+        self.sheet = service.spreadsheets()
+        self.SPREADSHEET_ID = SPREADSHEET_ID
 
-        # Call the Sheets API
-        sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
-        values = result.get('values', [])
+    def read(self, RANGE_NAME):
+        try:
+            # Call the Sheets API
+            result = self.sheet.values().get(spreadsheetId=self.SPREADSHEET_ID, range=RANGE_NAME).execute()
+            values = result.get('values', [])
 
-        if not values:
-            print('No data found.')
-            return
-        
-        return values
-    except HttpError as err:
-        print(err)
+            if not values:
+                print('No data found.')
+                return
+            
+            return values
+        except HttpError as err:
+            print(err)
 
-if __name__ == '__main__':
-    main()
+    def driver_info(self):
+        driverinfo = {}
+        drivertoteam = self.read("Race Results!A3:AD")
+        drivers = self.read("Drivers Standings!A2:C")
+        constructors = self.read("Constructor Standings!A2:C")
+        for i in drivertoteam:
+            if i == []:
+                continue
+            if i[0] != "TBA" or i[0] == '':
+                driverinfo[i[0]] = {"Team": i[1]}
+
+                wins = 0
+                podiums = 0
+                for r in i[4:]:
+                    if r == "x": continue
+                    if int(r) <= 3 & int(r): podiums += 1
+                    if int(r) == 1: wins += 1
+                driverinfo[i[0]]['Wins'] = wins
+                driverinfo[i[0]]['Podiums'] = podiums
+
+        for i in drivers:
+            if i[1] != "TBA":
+                driverinfo[i[1]]['Position'], driverinfo[i[1]]['Points'] = i[0], i[2]
+        for i in constructors:
+            for d in driverinfo.keys():
+                if i[1] == driverinfo[d]['Team']:
+                    if "TeamPosition" in driverinfo[d].keys():
+                        continue
+                    else: 
+                        driverinfo[d]['TeamPosition'] = i[0]
+                    if i[0]:
+                        driverinfo[d]['TeamPoints'] = i[2]
+
+        return driverinfo
+
+
+
--- main.py
+++ main.py
@@ -1,44 +1,105 @@
 import discord
-import get_data
+from discord.ext import commands
+from get_data import get_data
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='$f1 ')
+data = get_data("1fJmdaoYiMquDwgxFETxv2Ig4A_Qon9lZSsDwnR8malw")
 
-@client.event
+class botData:
+    botCommands = {
+        "help" : "Displays Help information about the Blackout F1 Bot",
+        "standings" : "Displays the current season standings by driver",
+        "constructors" : "Displays the current season standings by constructor",
+        "stats" : "Displays your stats as well as other driver's"
+    }
+    season = "Season 10"
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@bot.command(aliases=['s', 'drivers', 'd'])
+async def standings(ctx, clear=1, *args):
+    class driverStandings:
+        Position = []
+        Name = []
+        Points = []
+
+    standings = get_standings()
+    for value in standings:
+        if value[1] == "TBA":
+            continue
+        driverStandings.Position.append(value[0])
+        driverStandings.Name.append(value[1])
+        driverStandings.Points.append(value[2])
+
+    embedVar = discord.Embed(title="**Championship Standings**", colour=0xFFC300)
+    embedVar.set_author(name=f"Blackout F1 | {botData.season}", icon_url='https://i.imgur.com/KlHtdK3.jpg', url='https://bit.ly/BlackoutS8')
+    embedVar.add_field(name="Position", value="\n".join(driverStandings.Position), inline=True)
+    embedVar.add_field(name="Driver", value="\n".join(driverStandings.Name), inline=True)
+    embedVar.add_field(name="Points", value="\n".join(driverStandings.Points), inline=True)
+    embedVar.set_footer(text="Created by Tom", icon_url="https://i.imgur.com/ncL0qpO.png")
+    await ctx.message.reply(embed=embedVar)
+
+@bot.command(aliases=['c'])
+async def constructors(ctx, *args):
+    class constructorStandings:
+        Position = []
+        Name = []
+        Points = []
+    
+    constructors = get_constructors()
+    for value in constructors:
+        if value[1] == "Reserve Drivers*":
+            continue
+        constructorStandings.Position.append(value[0])
+        constructorStandings.Name.append(value[1])
+        constructorStandings.Points.append(value[2])
+
+    embedVar = discord.Embed(title="**Constructor Standings**", colour=0xFFC300)
+    embedVar.set_author(name=f"Blackout F1 | {botData.season}", icon_url='https://i.imgur.com/KlHtdK3.jpg', url='https://bit.ly/BlackoutS8')
+    embedVar.add_field(name="Position", value="\n".join(constructorStandings.Position), inline=True)
+    embedVar.add_field(name="Driver", value="\n".join(constructorStandings.Name), inline=True)
+    embedVar.add_field(name="Points", value="\n".join(constructorStandings.Points), inline=True)
+    embedVar.set_footer(text="Created by Tom", icon_url="https://i.imgur.com/ncL0qpO.png")
+    await ctx.message.reply(embed=embedVar)
+
+@bot.command()
+async def stats(ctx, *args):
+    driverinfo = driver_info()
+    if args == ():
+        user = str(ctx.author).split("#")[0]
+    else:
+        user = args[0]
+    try:
+        user_info = driverinfo[user]
+    except:
+        await ctx.message.reply(f"User: {user} not found in database (This is case sensitive, run $f1 s to list all drivers)")
         return
 
-    if message.content.startswith('$f1'):
-        class driverStandings:
-            Position = []
-            Name = []
-            Points = []
+    embedVar = discord.Embed(title=f"**{user}'s Stats**", colour=0xFFC300)
+    embedVar.set_author(name=f"Blackout F1 | {botData.season}", icon_url='https://i.imgur.com/KlHtdK3.jpg', url='https://bit.ly/BlackoutS8')
+    embedVar.add_field(name="Position", value=user_info['Position'], inline=True)
+    embedVar.add_field(name="Points", value=user_info['Points'], inline=True)
+    embedVar.add_field(name="Team", value=user_info['Team'], inline=True)
+    embedVar.add_field(name="Podiums", value=user_info['Podiums'], inline=True)
+    embedVar.add_field(name="Wins", value=user_info['Wins'], inline=True)
+    embedVar.add_field(name="Team's Position", value=user_info['TeamPosition'], inline=True)
+    embedVar.add_field(name="Team's Points", value=user_info['TeamPoints'], inline=True)
+    embedVar.set_footer(text="Created by Tom", icon_url="https://i.imgur.com/ncL0qpO.png")
+    await ctx.message.reply(embed=embedVar)
 
-        standings = get_standings()
-        for value in standings:
-            if value[1] == "TBA":
-                continue
-            driverStandings.Position.append(value[0])
-            driverStandings.Name.append(value[1])
-            driverStandings.Points.append(value[2])
-
-        values = get_data.main()
-        embedVar = discord.Embed(title="**Championship Standings**", colour=0xFFC300)
-        embedVar.set_author(name='Blackout F1 | Season 8', icon_url='https://i.imgur.com/KlHtdK3.jpg', url='https://bit.ly/BlackoutS8')
-        embedVar.add_field(name="Position", value="\n".join(driverStandings.Position), inline=True)
-        embedVar.add_field(name="Driver", value="\n".join(driverStandings.Name), inline=True)
-        embedVar.add_field(name="Points", value="\n".join(driverStandings.Points), inline=True)
-        embedVar.set_footer(text="Created by Tom", icon_url="https://i.imgur.com/ncL0qpO.png")
-
-        await message.channel.send(embed=embedVar)
 
 def get_standings():
-    standings = get_data.main()
+    standings = data.read("Drivers Standings!A2:C")
     return standings
 
+def get_constructors():
+    constructors = data.read("Constructor Standings!A2:C")
+    return constructors
 
-client.run('OTgxNjI2NDU2MzQzOTgyMDgx.GAaFit.L7_6LeZ1S9J9JDn9qqhzjt20DUDASfj400apj8')
+def driver_info():
+    driver_info = data.driver_info()
+    return driver_info
+
+bot.run('OTAzMzM1MzIyMzg4MTM1OTU2.GZVIx3.fTEYugaZX3ElvBhV3RneH3HeOG2AAyjrnS8V2w')
